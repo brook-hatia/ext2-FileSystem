14aac92f1bc1dc5b5b8acc839db66e16326a20ec
brook-hatia <brooktesfahun@gmail.com>
2023-10-11 14:12:31 -0500

    prompt added

--- shell/server.cpp
+++ shell/server.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <sys/socket.h> //library for server-client communication
 #include <netinet/in.h> //for serveaddr_in which is used for IPv4
-#include <arpa/inet.h>
-#include <unistd.h> //for close()
+#include <unistd.h>     //for close()
 using namespace std;
 
 int main()
@@ -14,58 +13,43 @@ int main()
     servaddr.sin_port = 8080;              // port number. "host to network short"
     servaddr.sin_addr.s_addr = INADDR_ANY; // bind to any available address
 
-    socklen_t addrlen = sizeof(servaddr); // size of the address
-    char rcvMsg[1024] = {0};              // allocated space for receiving from client
-    const char *sendMsg = "Hello world";  // message to send to client
-
-    // for server we need socket(), bind(), listen(), accept(), read(), write() function calls to connect with client
+    // for server we need socket(), bind(), listen(), accept(), read(), write() function calls
 
     int socketfd = socket(AF_INET, SOCK_STREAM, 0); // create socket. AF_INET for ipv4, SOCK_STREAM for TCP connection, 0 for protocol
-    if (socketfd == -1)
-    {
-        cout << "\nsocket creation failed";
-        exit(1);
-    }
-    cout << "\nsocket created";
 
-    int bindfd = bind(socketfd, (sockaddr *)&servaddr, addrlen); // bind to specific IP addr. servaddr is the specified IP addr, and addrlen is the length of the IP addr
-    if (bindfd == -1)
-    {
-        cout << "\nbind failed";
-        exit(1);
-    }
-    cout << "\nbind successful";
+    int bindfd = bind(socketfd, (sockaddr *)&servaddr, (socklen_t)sizeof(servaddr)); // bind to specific IP addr. servaddr is the specified IP addr, and the socket length of the servaddr in bytes
 
-    int listenfd = listen(socketfd, 5); // listen to incoming connection. 5 is the backlog of incoming message in queue
-    if (listenfd == -1)
-    {
-        cout << "\nlisten failed";
-        exit(1);
-    }
-    cout << "\nlisten successful";
+    int listenfd = listen(socketfd, 1); // listen to incoming connection. 1 is the backlog of incoming message in queue
 
-    int acceptfd = accept(socketfd, (struct sockaddr *)&servaddr, &addrlen); // new socket after connect() is called on client side
-    if (acceptfd == -1)
-    {
-        cout << "\naccept failed";
-        exit(1);
-    }
-    cout << "\naccept successful";
+    socklen_t servaddrLen = sizeof(servaddr);                                    // size of servaddr in bytes
+    int acceptfd = accept(socketfd, (struct sockaddr *)&servaddr, &servaddrLen); // new socket after connect() is called on client side
 
-    int writefd = write(acceptfd, sendMsg, strlen(sendMsg)); // send message to client
-    if (writefd == -1)
-    {
-        cout << "\nwrite failed";
-        exit(1);
+    if (socketfd == -1 || bindfd == -1 || listenfd == -1 || acceptfd == -1)
+    { // error returns -1
+        cout << "connection failed" << endl;
+        return 1;
     }
-    cout << "\nwrite successful" << rcvMsg;
+    cout << "connection success" << endl;
 
-    int readfd = read(acceptfd, rcvMsg, sizeof(rcvMsg)); // receive message from client
-    if (readfd == -1)
+    char sendMsg[100]; // allocate space for send message
+
+    while (true)
     {
-        cout << "\nread failed";
-        exit(1);
+        char readMsg[3000] = {0};                              // allocated space for receiving from client
+        int readfd = read(acceptfd, readMsg, sizeof(readMsg)); // receive message from client
+        cout << "\nClient: " << readMsg << endl;
+
+        cout << "Server shell: ";
+        cin >> sendMsg;
+        string str = sendMsg;
+        if (str == "exit")
+        {
+            break;
+        }
+
+        int writefd = write(acceptfd, sendMsg, strlen(sendMsg)); // send message to client
+        // cout << "\nwrite successful" << sendMsg;
     }
-    cout << "\nread successful" << rcvMsg;
+
     close(socketfd); // close client's socket
 }
